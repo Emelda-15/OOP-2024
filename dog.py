b8a63fcb0b407b58e4161a246473fdc45bef94ea
Emelda-15 <139153435+Emelda-15@users.noreply.github.com>
2024-09-24 10:11:23 +0300

    Add files via upload

---
+++ dog.py
@@ -0,0 +1,21 @@
+class Dog:
+    # Constructor (Initializer)
+    def __init__(self, name, breed, age):
+        self.name = name  
+        self.breed = breed 
+        self.age = age  
+        
+        def play(self):
+          print(f"The dog is playing")
+        
+        def show(self):
+            print(f"Name: {self.name}")
+            print(f"Breed: {self.breed}")
+            print(f"Gender: {self.gender}")
+            print(f"Birthday: {self.birthday}")
+            
+Dave =Dog("Dave", "maltiz", "female")
+Dave.info()
+            
+    
+    
